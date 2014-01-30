4151f62cb13451eeca8bf2aa032712695f5c2441
petehunt <floydophone@gmail.com>
2014-01-30 15:54:03 -0800

    add frontend

--- tictactoe.js
+++ tictactoe.js
@@ -2,6 +2,8 @@ if (Meteor.isClient) {
   Meteor.startup(function() {
     Deps.autorun(function() {
       board = new Board("Pete's game");
+
+      React.renderComponent(renderBoard(board), document.body);
     });
   });
 }
